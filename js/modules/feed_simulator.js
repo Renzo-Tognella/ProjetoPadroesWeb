959f4026277cc953b5a6e0d3330efb79d207b951
Renzo <renzotognella@alunos.utfpr.edu.br>
2025-10-18 14:31:04 -0300

    Cria estrutura do projeto inicial, define readme, define css global

---
+++ js/modules/feed_simulator.js
@@ -0,0 +1,6 @@
+/**
+ * feed_simulator.js
+ * 
+ * Lógica do simulador de "feed" de redes sociais
+ * Usado na Página Algoritmo para demonstrar como funcionam os feeds viciantes
+ */
---
+++ js/modules/ux_disruption.js
@@ -0,0 +1,7 @@
+/**
+ * ux_disruption.js
+ * 
+ * Módulo para manipulação de eventos disruptivos
+ * Implementa botões que mudam de lugar, links aleatórios e outros elementos desafiadores
+ */
+
